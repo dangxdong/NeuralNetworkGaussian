019fd957189b543bbd1b4702f2ad6813b0d4dff6
Xiaodong Dang <dangxdong@hotmail.com>
2016-03-21 21:23:29 +0000

    Added files via upload

---
+++ nnGaussianLogistic.m
@@ -0,0 +1,30 @@
+function [Theta1, Theta2, minicost] = nnGaussianLogistic(X, y, ...
+                           hidden_layer_size = 0, lambda = 0, maxIter = 1000)
+
+input_layer_size  = size(X,2);
+num_labels = size(y,2);  
+if (hidden_layer_size == 0) 
+    hidden_layer_size = round((input_layer_size+num_labels)/2);
+end
+
+initial_Theta1 = randInitGradGaussian(input_layer_size, hidden_layer_size);
+initial_Theta2 = randInitGrad(hidden_layer_size+1, num_labels);
+initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
+
+%  Train the model.
+options = optimset('MaxIter', maxIter);
+costFunction = @(p) nnCostFuncGaussianLogistic(p, ...
+                                   input_layer_size, ...
+                                   hidden_layer_size, ...
+                                   num_labels, X, y, lambda);
+[nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+
+Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 2)), ...
+                 hidden_layer_size, (input_layer_size + 2));
+
+Theta2 = reshape(nn_params((1 + hidden_layer_size * (input_layer_size + 2)):end), ...
+                 num_labels, (hidden_layer_size + 2));
+
+minicost = cost(end);				 
+
+end
---
+++ predictCaussianLogi.m
@@ -0,0 +1,20 @@
+function [p pn]= predictCaussianLogi(Theta1, Theta2, X)
+
+% Returns the predicted count of y given the
+% trained weights of a neural network (Theta1, Theta2)
+
+m = size(X, 1);
+
+% input to hidden layer, gaussian regression
+x1 = [ones(m, 1) X ones(m, 1)];
+z =  Theta1 * x1';
+h1 = gaussian(z);
+h1 = [ones(1, m); h1;ones(1, m)];
+% hidden layer to output: logistic regression
+z2 = Theta2 * h1;
+h2 = sigmoid(z2);
+% just return the h2 values. 
+p = h2';
+pn = round(p);
+
+end
